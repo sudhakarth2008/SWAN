a877f4295c1f12574c2f2f28b7c53a18d7a6c2f2
Venki <Venki@Venki-PC>
2019-04-02 09:35:24 +0530

    SWAN total project

---
+++ SWAN/src/main/java/SWAN/SWAN/App.java
@@ -0,0 +1,13 @@
+package SWAN.SWAN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SWAN/src/test/java/OrangeHrm/OR.java
@@ -0,0 +1,62 @@
+package OrangeHrm;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class OR {
+	
+
+public static String sTargetTestCaseExcelPath="";	
+public static String sTargetFolderPath="";	
+public static String sTestCaseName="";	
+
+public static String username_sendkey="//input[@id='txtUsername']";
+public static String password_sendkey="//input[@id='txtPassword']";
+public static String login_click="//input[@id='btnLogin']";
+
+//recruitment starts
+
+public static String Recruitment_focus="//a[@id='menu_recruitment_viewRecruitmentModule']";
+
+public static String vacancies_click="//a[@id='menu_recruitment_viewJobVacancy']";
+
+public static String AddButton_click="//input[@id='btnAdd']"; 
+
+public static String AddJobVacancy_jobTitle_listbox="//select[@id='addJobVacancy_jobTitle']";
+
+public static String AddJobVacancy_name_sendkeys="//input[@id='addJobVacancy_name']";
+
+public static String AddJobVacancy_hiringManager_sendkeys="//input[@id='addJobVacancy_hiringManager']";
+
+
+public static String AddJobVacancy_noofPositions_sendkeys="//input[@id='addJobVacancy_noOfPositions']";
+
+public static String AddJobVacancy_save_click="//input[@id='btnSave']"; 
+
+public static String Vacancies_Add_Backbutton_click="//input[@id='btnBack']";
+
+
+
+//vacancy search starts
+public static String vacancy_listbox="//select[@id='vacancySearch_jobVacancy']";
+
+public static String Vacancies_search_click="//input[@id='btnSrch']";
+
+public static String Vacancies_columns="//*[@id='resultTable']/thead/tr/th";
+
+public static String Vacancies_Rows="//*[@id='resultTable']/tbody/tr/td[2]";
+
+//vacancy search Ends
+
+//public static String jobtitle_listbox="//select[@id='vacancySearch_jobTitle']";
+
+
+//public static String HiringManager_listbox="//select[@id='vacancySearch_hiringManager']";
+
+
+//public static String status_listbox="//select[@id='vacancySearch_status']";
+
+
+
+}
+
